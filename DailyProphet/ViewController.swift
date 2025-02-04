db6cf7f4549d71476472db549ee9828bc3b79550
Ana oliveira <ana.oliveira@sky.uk>
2025-02-04 10:33:14 +0000

    First draft

---
+++ DailyProphet/ViewController.swift
@@ -0,0 +1,140 @@
+import UIKit
+import ARKit
+import SceneKit
+
+class ViewController: UIViewController {
+
+    var sceneView: ARSCNView!  // This will be the ARSCNView that displays the AR content.
+    let videoNode = SKVideoNode(fileNamed: "HarryPotter\(Constants.movieExtension)")
+
+    // Constants for configuration and media files
+    private enum Constants {
+        static let imageToRecognizeFolder: String = "ImagesToRecognize"  // Folder containing reference images to track
+        static let movieExtension: String = ".mp4"  // Extension for the video files
+        static let maximumNumberOfTrackedImages: Int = 1  // Maximum number of images to track at once
+        static let videoSceneHeight: Int = 360  // Height for the video scene
+        static let videoSceneWidth: Int = 480  // Width for the video scene
+    }
+
+    // This method is called when the view is loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureSceneView()  // Calls function to set up AR scene view
+    }
+
+    // This method is called when the view is about to appear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        activeARWorldTracking()  // Starts AR world tracking with image recognition
+    }
+
+    // This method is called when the view is about to disappear
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        pauseARWorldTracking()  // Pauses AR session tracking
+    }
+}
+
+// MARK: - ViewController Private functions
+extension ViewController {
+
+    // Sets up the ARSCNView
+    private func configureSceneView() {
+        sceneView = ARSCNView(frame: view.bounds)  // Creates a new ARSCNView with the same bounds as the view
+        sceneView.autoresizingMask = [.flexibleWidth, .flexibleHeight]  // Ensures the AR view resizes with the screen
+        sceneView.delegate = self  // Sets the ViewController as the delegate for ARSCNView
+        view.addSubview(sceneView)  // Adds the sceneView to the view hierarchy
+        sceneView.showsStatistics = false  // Disables AR statistics display
+    }
+
+    // Starts AR session with image recognition
+    private func activeARWorldTracking() {
+        let configuration = ARWorldTrackingConfiguration()  // Creates a new world tracking configuration
+
+        // Loads reference images from the specified folder in the app bundle
+        if let imagesToTrack = ARReferenceImage.referenceImages(
+            inGroupNamed: Constants.imageToRecognizeFolder,
+            bundle: nil
+        ) {
+            configuration.detectionImages = imagesToTrack  // Sets the images to be tracked by ARKit
+            configuration.maximumNumberOfTrackedImages = Constants.maximumNumberOfTrackedImages  // Limits the number of images being tracked
+        }
+
+        sceneView.session.run(configuration)  // Starts the AR session with the configured settings
+    }
+
+    // Pauses the AR session
+    private func pauseARWorldTracking() {
+        sceneView.session.pause()  // Pauses the AR session when the view disappears
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+
+    // This delegate method is called whenever a new anchor is added to the AR session
+    func renderer(
+        _ renderer: SCNSceneRenderer,
+        nodeFor anchor: ARAnchor
+    ) -> SCNNode? {
+        // Checks if the anchor is an image anchor (meaning it's a recognized image)
+        guard
+            let imageAnchor = anchor as? ARImageAnchor,
+            let videoScene = videoScene(imageAnchor: imageAnchor)  // Calls videoScene to get the SKScene for the video
+        else {
+            return nil  // If it's not an image anchor, return nil (nothing to do)
+        }
+
+        // Creates a plane node to display the video
+        let plane = SCNPlane(
+            width: imageAnchor.referenceImage.physicalSize.width,  // Width of the plane based on the real image's size
+            height: imageAnchor.referenceImage.physicalSize.height  // Height of the plane based on the real image's size
+        )
+        plane.firstMaterial?.diffuse.contents = videoScene  // Sets the video scene as the material for the plane
+
+        // Create a node for the plane and rotate it so it faces the camera
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2  // Rotates the plane by 90 degrees to face the camera
+
+        let node = SCNNode()  // Create a new node to hold the plane node
+        node.addChildNode(planeNode)  // Adds the plane node to the new node
+
+        return node  // Returns the node to be displayed in the AR scene
+    }
+
+    func renderer(_ renderer: any SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard
+            let imageAnchor = anchor as? ARImageAnchor,
+            !imageAnchor.isTracked
+        else {
+            videoNode.play()
+            return
+        }
+        videoNode.pause()
+    }
+
+    // This function creates a video scene to display on the AR image
+    private func videoScene(imageAnchor : ARImageAnchor) -> SKScene? {
+
+        // Creates a new SKScene to hold the video
+        let videoScene = SKScene(
+            size: CGSize(
+                width: Constants.videoSceneWidth,  // Width of the video scene
+                height: Constants.videoSceneHeight  // Height of the video scene
+            )
+        )
+
+        // Positions the video in the center of the scene
+        videoNode.position = CGPoint(
+            x: videoScene.size.width / 2,
+            y: videoScene.size.height / 2
+        )
+        videoNode.yScale = -1.0  // Flips the video vertically to match AR coordinate system
+        videoScene.addChild(videoNode)  // Adds the video node to the scene
+
+        return videoScene  // Returns the created video scene
+    }
+}
